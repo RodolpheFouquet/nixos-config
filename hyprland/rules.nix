c9dbc07d42c55506b50af1bb89648b19f9115e4f
Rodolphe Fouquet <rodolphe@unicowd.com>
2025-07-11 02:31:22 +0100

    fix window rules

--- hyprland/rules.nix
+++ hyprland/rules.nix
@@ -1,19 +1,20 @@
-{...}:{
+{ ... }:
+{
 
-  wayland.windowManager.hyprland.settings ={
-      windowrule = [
-        "suppressevent maximize, class:.*"
-        "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
-      ];
+  wayland.windowManager.hyprland.settings = {
+    windowrule = [
+      "suppressevent maximize, class:.*"
+      "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+    ];
 
-      windowrulev2 = [
-        "workspace 1, class:^(google-chrome)$"
-        "workspace 1, class:^(com.mitchellh.ghostty)$"
-        "workspace 2, class:^(discord)$, silent"
-        "workspace 3, class:^(steam)$, silent"
-        "workspace 4, class:^(steam_app_.*)$"
-        "workspace 4, xwayland:1,class:^(gamescope)$"
-      ];
+    windowrulev2 = [
+      "workspace 1, class:^(google-chrome)$"
+      "workspace 1, class:^(com.mitchellh.ghostty)$"
+      "workspace 2 silent, class:^(discord)$"
+      "workspace 3 silent, class:(steam)$"
+      "workspace 4, class:^(steam_app_.*)$"
+      "workspace 4, xwayland:1,class:^(gamescope)$"
+    ];
 
   };
 }
